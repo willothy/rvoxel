a0e5face4be8d8eebe57c2e3f9cb73ac974d070b
Will Hopkins <willothyh@gmail.com>
2025-09-04 02:17:56 -0700

    create swapchain

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,68 @@
 use std::{mem::MaybeUninit, sync::atomic::AtomicBool};
 
-use raw_window_handle::HasDisplayHandle;
+use ash::{khr::surface, vk};
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use winit::{application::ApplicationHandler, event_loop::EventLoop, window::Window};
 
 pub struct VulkanAppInner {
+    /// The window that we are rendering to, from [`winit`].
     window: winit::window::Window,
+
+    /// The Vulkan instance.
+    ///
+    /// TODO: figure out and explain what this really is
     instance: ash::Instance,
+
+    /// Physical device represents a GPU in the system that we have
+    /// selected.
     physical_device: ash::vk::PhysicalDevice,
+
+    /// The logical device is our connection to the physical device.
+    ///
+    /// You can think of it like a session between the application and the GPU driver.
     device: ash::Device,
+
+    /// Queue for submitting graphics commands.
     graphics_queue: ash::vk::Queue,
+
+    /// The platform-specific window or display that we are rendering to.
+    surface: ash::vk::SurfaceKHR,
+    surface_loader: surface::Instance,
+
+    /// The swap chain is Vulkan's version of double buffering.
+    ///
+    /// Where in OpenGL you would just render to the back buffer and call swap_buffers(),
+    /// in Vulkan you manage 2-3 images and explicitly manage rendering to them and presenting
+    /// them.
+    swapchain: vk::SwapchainKHR,
+    swapchain_loader: ash::khr::swapchain::Device,
+    /// The swapchain images.
+    swapchain_images: Vec<vk::Image>,
+    /// The format of the images in the swapchain.
+    swapchain_format: vk::Format,
+    /// The extent (width and height) of the swapchain images.
+    swapchain_extent: vk::Extent2D,
 }
 
 pub struct VulkanApp {
     app: MaybeUninit<VulkanAppInner>,
     initialized: AtomicBool,
+}
+
+impl Drop for VulkanApp {
+    fn drop(&mut self) {
+        if self.is_initialized() {
+            unsafe {
+                std::ptr::drop_in_place(self.app.as_mut_ptr());
+            }
+        }
+    }
+}
+
+impl Drop for VulkanAppInner {
+    fn drop(&mut self) {
+        // TODO: cleanup Vulkan resources
+    }
 }
 
 impl VulkanApp {
@@ -51,12 +100,34 @@ impl VulkanApp {
         let (device, graphics_queue) =
             unsafe { Self::create_logical_device(&instance, &physical_device)? };
 
+        let (surface, surface_loader) = unsafe { Self::create_surface(entry, &instance, &window)? };
+
+        let (swapchain, swapchain_loader, surface_format, extent) = unsafe {
+            Self::create_swap_chain(
+                &instance,
+                &physical_device,
+                &device,
+                &surface,
+                &surface_loader,
+                &window,
+            )?
+        };
+
         let app_inner = VulkanAppInner {
             window,
             instance,
             physical_device,
             device,
             graphics_queue,
+
+            surface,
+            surface_loader,
+
+            swapchain,
+            swapchain_format: surface_format.format,
+            swapchain_extent: extent,
+            swapchain_images: unsafe { swapchain_loader.get_swapchain_images(swapchain)? },
+            swapchain_loader,
         };
 
         self.app = MaybeUninit::new(app_inner);
@@ -75,6 +146,123 @@ impl VulkanApp {
         uninit.initialize(entry, ev)?;
 
         Ok(uninit)
+    }
+
+    unsafe fn create_swap_chain(
+        instance: &ash::Instance,
+        physical_device: &ash::vk::PhysicalDevice,
+        device: &ash::Device,
+        surface: &ash::vk::SurfaceKHR,
+        surface_loader: &surface::Instance,
+        window: &Window,
+    ) -> anyhow::Result<(
+        vk::SwapchainKHR,
+        ash::khr::swapchain::Device,
+        vk::SurfaceFormatKHR,
+        vk::Extent2D,
+    )> {
+        let capabilities = unsafe {
+            surface_loader.get_physical_device_surface_capabilities(
+                physical_device.clone(),
+                surface.clone(),
+            )?
+        };
+        let formats = unsafe {
+            surface_loader
+                .get_physical_device_surface_formats(physical_device.clone(), surface.clone())?
+        };
+        let present_modes = unsafe {
+            surface_loader.get_physical_device_surface_present_modes(
+                physical_device.clone(),
+                surface.clone(),
+            )?
+        };
+
+        // Pick image count (2-3 is good)
+        let image_count =
+            (capabilities.min_image_count + 1).min(if capabilities.max_image_count > 0 {
+                capabilities.max_image_count
+            } else {
+                u32::MAX
+            });
+
+        // Pick format (first one is usually fine)
+        let surface_format = formats[0];
+
+        // Pick present mode
+        //
+        // FIFO is always supported, but try MAILBOX if available
+        let present_mode = if present_modes.contains(&vk::PresentModeKHR::MAILBOX) {
+            vk::PresentModeKHR::MAILBOX // Triple buffering
+        } else {
+            vk::PresentModeKHR::FIFO // VSync
+        };
+
+        let swapchain_device = ash::khr::swapchain::Device::new(&instance, &device);
+
+        let info = vk::SwapchainCreateInfoKHR::default()
+            .surface(surface.clone())
+            .min_image_count(image_count)
+            .image_format(surface_format.format)
+            .image_color_space(surface_format.color_space)
+            .image_extent(capabilities.current_extent)
+            // Always 1 unless doing VR
+            .image_array_layers(1)
+            // We'll draw to these
+            .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+            // Only graphics queue uses them
+            .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+            // No rotation
+            .pre_transform(capabilities.current_transform)
+            // No transparency
+            .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+            .present_mode(present_mode)
+            // Don't care about pixels behind other windows
+            .clipped(true);
+
+        // use ash::extensions::khr::swapchain;
+        let swap_chain = unsafe { swapchain_device.create_swapchain(&info, None)? };
+
+        let extent = if capabilities.current_extent.width != u32::MAX {
+            // Surface tells us exactly what size to use
+            capabilities.current_extent
+        } else {
+            // We need to figure out the size from the window
+            let window_size = window.inner_size();
+
+            vk::Extent2D {
+                width: window_size.width.clamp(
+                    capabilities.min_image_extent.width,
+                    capabilities.max_image_extent.width,
+                ),
+                height: window_size.height.clamp(
+                    capabilities.min_image_extent.height,
+                    capabilities.max_image_extent.height,
+                ),
+            }
+        };
+
+        Ok((swap_chain, swapchain_device, surface_format, extent))
+    }
+
+    unsafe fn create_surface(
+        entry: &ash::Entry,
+        instance: &ash::Instance,
+        window: &Window,
+    ) -> anyhow::Result<(ash::vk::SurfaceKHR, surface::Instance)> {
+        let surface = unsafe {
+            ash_window::create_surface(
+                entry,
+                instance,
+                window.display_handle()?.into(),
+                window.window_handle()?.into(),
+                None,
+            )?
+        };
+
+        let surface_loader = surface::Instance::new(entry, &instance);
+
+        Ok((surface, surface_loader))
     }
 
     unsafe fn pick_physical_device(
